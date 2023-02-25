6af238362fad7d5ca9d6c153684b111c63352df0
Tiago da silva Pereira <tiago.pereira@supplylabs.com.br>
2023-02-25 14:31:06 -0300

    input e filtro de livros

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,9 @@
+import livro from "../../imagens/livro.png";
+
+export const livros = [
+  { nome: "Livro 1", id: 1, src: livro },
+  { nome: "Livro 2", id: 2, src: livro },
+  { nome: "Livro 3", id: 3, src: livro },
+  { nome: "Livro 4", id: 4, src: livro },
+  { nome: "Livro 5", id: 5, src: livro },
+];
---
+++ src/componentes/Pesquisa/index.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { useState } from "react";
+import { livros } from "./dadosPesquisa";
+import styled from "styled-components";
+import { Input } from "../Inout";
+
+const PesquisaContainer = styled.section`
+  background-image: linear-gradient(90deg, #002f52 35%, #325489 165%);
+  color: #fff;
+  text-align: center;
+  padding: 85px 0;
+  height: 470px;
+  width: 100%;
+`;
+const Titulo = styled.h2`
+  color: #fff;
+  font-size: 36px;
+  text-align: center;
+  width: 100%;
+`;
+const Subtitulo = styled.h3`
+  font-size: 16px;
+  font-weight: 500;
+  margin-bottom: 40px;
+`;
+const Resultado = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  cursor: pointer;
+  p {
+    width: 200px;
+  }
+
+  img {
+    width: 100px;
+  }
+
+  &:hover {
+    border: 1px solid white;
+  }
+`;
+
+export default function Pesquisa() {
+  const [livrosPesquisados, setLivrosPesquisados] = useState([]);
+
+  const fazPesquisa = (event) => {
+    const textoDigitado = event.target.value;
+    const resultadoPesquisa = livros.filter((livro) =>
+      livro.nome.includes(textoDigitado)
+    );
+
+    setLivrosPesquisados(resultadoPesquisa);
+  };
+
+  return (
+    <PesquisaContainer>
+      <Titulo>Já Sabe por onde começar?</Titulo>
+      <Subtitulo>Encontre seu livro em nossa estante</Subtitulo>
+      <Input
+        placeholder="Escreva sua próxima leitura"
+        onBlur={(e) => fazPesquisa(e)}
+      />
+      {livrosPesquisados.map((livro) => (
+        <Resultado>
+          <img src={livro.src} alt="capa do livro" />
+          <p>{livro.nome}</p>
+        </Resultado>
+      ))}
+    </PesquisaContainer>
+  );
+}
